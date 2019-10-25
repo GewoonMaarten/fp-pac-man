ccac5bd6feb70ef8c518239c969c797e5d551779
Maarten de Klerk <maarten.deklerk@gmail.com>
2019-10-25 16:50:17 +0200

    Added temp functions to undefined functions

--- src/Main.hs
+++ src/Main.hs
@@ -28,11 +28,11 @@ draw = return . drawPure
 
 
 drawPure :: GameState -> Picture
-drawPure g = pictures ((showGrid $ unLevel g) (-180) (-230))
+drawPure g = pictures (showGrid (unLevel g) (-200) 250)
 
 input :: Event -> GameState -> IO GameState
-input = undefined
+input _ gstate = return gstate
 
 
 update :: Float -> GameState -> IO GameState
-update = undefined
+update _ gstate = return gstate
