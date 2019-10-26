8a647a3118ab6e7c31d5a6d5b1b729564908bdc9
Maarten de Klerk <maarten.deklerk@gmail.com>
2019-10-26 16:38:51 +0200

    Update level

--- src/Level.hs
+++ src/Level.hs
@@ -1,102 +1,101 @@
 module Level where
 
-import Graphics.Gloss
+import           Graphics.Gloss
 
-type Grid = [[GridItem]] 
+type Grid = [[GridItem]]
 
-data CollectibleType = PacDot 
-    | Energizer 
-    | Fruit 
+data CollectibleType = PacDot
+    | Energizer
+    | Fruit
     deriving (Show)
 
-data CollectibleState = Collected 
-    | Available 
+data CollectibleState = Collected
+    | Available
     deriving (Show)
 
-data GridItem = Empty 
+data GridItem = Empty
     | Door
-    | Wall 
-    | SpawnPoint 
+    | Wall
+    | SpawnPoint
     | Collectible CollectibleState CollectibleType
     deriving (Show)
 
 gridSize = 20
 
 initGrid :: [[Int]]
-initGrid = [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-            [1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1],
-            [1,0,1,1,0,1,1,1,0,1,0,1,1,1,0,1,1,0,1],
-            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-            [1,0,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,0,1],
-            [1,0,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,0,1],
-            [1,1,1,1,0,1,1,1,0,1,0,1,1,1,0,1,1,1,1],
-            [1,1,1,1,0,1,0,0,0,0,0,0,0,1,0,1,1,1,1],
-            [1,1,1,1,0,1,0,1,1,3,1,1,0,1,0,1,1,1,1],
-            [0,0,0,0,0,0,0,1,2,2,2,1,0,0,0,0,0,0,0],
-            [1,1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,1],
-            [1,1,1,1,0,1,0,0,0,0,0,0,0,1,0,1,1,1,1],
-            [1,1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,1],
-            [1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1],
-            [1,0,1,1,0,1,1,1,0,1,0,1,1,1,0,1,1,0,1],
-            [1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1],
-            [1,1,0,1,0,1,0,1,1,1,1,1,0,1,0,1,0,1,1],
-            [1,0,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,0,1],
-            [1,0,1,1,1,1,1,1,0,1,0,1,1,1,1,1,1,0,1],
-            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-            [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]]
+initGrid =
+    [ [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    , [1, 4, 4, 4, 4, 4, 4, 4, 4, 1, 4, 4, 4, 4, 4, 4, 4, 4, 1]
+    , [1, 4, 1, 1, 4, 1, 1, 1, 4, 1, 4, 1, 1, 1, 4, 1, 1, 4, 1]
+    , [1, 4, 4, 5, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 4, 4, 1]
+    , [1, 4, 1, 1, 4, 1, 4, 1, 1, 1, 1, 1, 4, 1, 4, 1, 1, 4, 1]
+    , [1, 4, 4, 4, 4, 1, 4, 4, 4, 1, 4, 4, 4, 1, 4, 4, 4, 4, 1]
+    , [1, 1, 1, 1, 4, 1, 1, 1, 4, 1, 4, 1, 1, 1, 4, 1, 1, 1, 1]
+    , [1, 1, 1, 1, 4, 1, 0, 0, 0, 0, 0, 0, 0, 1, 4, 1, 1, 1, 1]
+    , [1, 1, 1, 1, 4, 1, 0, 1, 1, 3, 1, 1, 0, 1, 4, 1, 1, 1, 1]
+    , [4, 4, 4, 4, 4, 4, 0, 1, 2, 2, 2, 1, 0, 4, 4, 4, 4, 4, 4]
+    , [1, 1, 1, 1, 4, 1, 0, 1, 1, 1, 1, 1, 0, 1, 4, 1, 1, 1, 1]
+    , [1, 1, 1, 1, 4, 1, 0, 0, 0, 0, 0, 0, 0, 1, 4, 1, 1, 1, 1]
+    , [1, 1, 1, 1, 4, 1, 4, 1, 1, 1, 1, 1, 4, 1, 4, 1, 1, 1, 1]
+    , [1, 4, 4, 4, 4, 4, 4, 4, 4, 1, 4, 4, 4, 4, 4, 4, 4, 4, 1]
+    , [1, 4, 1, 1, 4, 1, 1, 1, 4, 1, 4, 1, 1, 1, 4, 1, 1, 4, 1]
+    , [1, 4, 4, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 4, 4, 1]
+    , [1, 1, 4, 1, 4, 1, 4, 1, 1, 1, 1, 1, 4, 1, 4, 1, 4, 1, 1]
+    , [1, 4, 4, 5, 4, 1, 4, 4, 4, 1, 4, 4, 4, 1, 4, 5, 4, 4, 1]
+    , [1, 4, 1, 1, 1, 1, 1, 1, 4, 1, 4, 1, 1, 1, 1, 1, 1, 4, 1]
+    , [1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1]
+    , [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    ]
 
 wallColor = color (makeColorI 23 24 254 255)
-dotColor  = color (makeColorI 254 169 164 255)
+dotColor = color (makeColorI 254 169 164 255)
 
 class Drawable a where
     drawItem :: a -> Float -> Float -> Picture
 
 instance Drawable GridItem where
     -- Nothing
-    drawItem Empty x y = translate x y 
-        $ color black 
-        $ rectangleSolid gridSize gridSize
+    drawItem Empty x y =
+        translate x y $ color black $ rectangleSolid gridSize gridSize
     -- Wall
-    drawItem Wall x y = translate x y 
-        $ wallColor 
-        $ rectangleSolid gridSize gridSize
+    drawItem Wall x y =
+        translate x y $ wallColor $ rectangleSolid gridSize gridSize
     -- Door
-    drawItem Door x y = translate x y 
-        $ color black 
-        $ rectangleSolid gridSize gridSize
+    drawItem Door x y =
+        let doorSize = 5
+        in  translate x y $ color red $ rectangleSolid gridSize doorSize
     -- SpawnPoint
-    drawItem SpawnPoint x y = translate x y 
-        $ color black 
-        $ rectangleSolid gridSize gridSize
+    drawItem SpawnPoint x y =
+        translate x y $ color black $ rectangleSolid gridSize gridSize
     -- Pac-Dot
-    drawItem (Collectible Available PacDot) x y = translate x y 
-        $ dotColor 
-        $ rectangleSolid (gridSize / 2) (gridSize / 2)
+    drawItem (Collectible Available PacDot) x y =
+        translate x y $ dotColor $ rectangleSolid (gridSize / 4) (gridSize / 4)
     -- Energizer
-    drawItem (Collectible Available Energizer) x y = translate x y 
-        $ dotColor 
-        $ circleSolid (gridSize / 2)
+    drawItem (Collectible Available Energizer) x y =
+        translate x y $ dotColor $ circleSolid (gridSize / 4)
     -- Fruit
-    drawItem (Collectible Available Fruit) x y = translate x y 
-        $ color red 
-        $ circleSolid (gridSize / 2)
+    drawItem (Collectible Available Fruit) x y =
+        translate x y $ color red $ circleSolid (gridSize / 2)
 
 -- Parse to int list to an grid
 buildGrid :: [[Int]] -> Grid
 buildGrid = map buildRow
-    where 
-        buildRow :: [Int] -> [GridItem]
-        buildRow []      = []
-        buildRow (0:xs)  = Empty : (buildRow xs)
-        buildRow (1:xs)  = Wall : (buildRow xs)
-        buildRow (2:xs)  = SpawnPoint : (buildRow xs)
-        buildRow (3:xs)  = Door : (buildRow xs) 
+  where
+    buildRow :: [Int] -> [GridItem]
+    buildRow []       = []
+    buildRow (1 : xs) = Wall : buildRow xs
+    buildRow (2 : xs) = SpawnPoint : buildRow xs
+    buildRow (3 : xs) = Door : buildRow xs
+    buildRow (4 : xs) = Collectible Available PacDot : buildRow xs
+    buildRow (5 : xs) = Collectible Available Energizer : buildRow xs
+    buildRow (_ : xs) = Empty : buildRow xs
 
 -- Parse the grid to a list of pictures with coordinates for an offset       
 showGrid :: Grid -> Float -> Float -> [Picture]
 showGrid [] _ _ = []
-showGrid (gis:grid) x y = (showRow gis x y) ++ (showGrid grid x (y - gridSize))
-    where
-        showRow :: [GridItem] -> Float -> Float -> [Picture]
-        showRow [] _ _ = []
-        showRow (g:gis) x y = (drawItem g x y) : (showRow gis (x + gridSize) y)
+showGrid (gis : grid) x y =
+    (showRow gis x y) ++ (showGrid grid x (y - gridSize))
+  where
+    showRow :: [GridItem] -> Float -> Float -> [Picture]
+    showRow []        _ _ = []
+    showRow (g : gis) x y = drawItem g x y : showRow gis (x + gridSize) y
