325503c8859f91676aefbbef755bdc805bc1aec3
Tom de Goede <tom.degoede@afas.nl>
2019-10-29 12:15:39 +0100

    Fix flickering image

--- src/PacMan.hs
+++ src/PacMan.hs
@@ -6,16 +6,15 @@ import           Graphics.Gloss.Data.Point.Arithmetic as Pt
 
 -- Path Logic
 upcomingNode :: [PathNode] -> Float -> (Int, Int) 
-upcomingNode [Pn x y] d = (x, y)
-upcomingNode (a:b:_)  d = (round x, round y)
-  where (x, y) = actualLocation [a, b] (fromIntegral $ floor d)
+upcomingNode pns d = (round x, round y)
+  where (x, y) = actualLocation pns (fromIntegral $ floor d)
 
 actualLocation [p] _     = toVector p
 actualLocation (a:b:_) d = vb Pt.+ (d Pt.* n)
   where
     vb = toVector b
     delta = (toVector a) Pt.- vb
-    n = normalizeV delta
+    n = if delta == (0,0) then (0,0) else normalizeV delta
 
 toVector :: PathNode -> Vector
 toVector (Pn x y) = (fromIntegral x, fromIntegral y)
