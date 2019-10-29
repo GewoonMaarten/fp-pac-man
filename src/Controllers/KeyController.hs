ff48d2058c37356160f4dc46b644719bd140fa00
Maarten de Klerk <maarten.deklerk@gmail.com>
2019-10-29 16:40:15 +0100

    Fixed grid position

--- src/Controllers/KeyController.hs
+++ src/Controllers/KeyController.hs
@@ -7,14 +7,14 @@ import           Model
 import           PacMan
 import           Controllers.Level
 import           Graphics.Gloss.Interface.IO.Game
-import           Data.Fixed(mod')
+import           Data.Fixed                     ( mod' )
 
 inputHandler :: Event -> GameState -> GameState
 -- Enter Key
 inputHandler (EventKey (SpecialKey KeyEnter) _ _ _) gameState =
   case unScene gameState of
     Play     -> gameState
-    Home     -> gameState { unScene = Play, unLevel = buildGrid initialGrid (-200) (-165) 20 }
+    Home -> gameState { unScene = Play, unLevel = buildGrid (-200) (-245) 20 }
     Pause    -> gameState { unScene = Home }
     GameOver -> gameState { unScene = Home }
 -- Esc Key
@@ -25,35 +25,38 @@ inputHandler (EventKey (SpecialKey KeyEsc) _ _ _) gameState =
     Home     -> gameState
     GameOver -> gameState
 -- Left Key
-inputHandler (EventKey (SpecialKey KeyLeft) _ _ _) gameState = scene Play (moveFn (\(x, y) -> (x - 1, y))) gameState
+inputHandler (EventKey (SpecialKey KeyLeft) _ _ _) gameState =
+  scene Play (moveFn (\(x, y) -> (x - 1, y))) gameState
 -- Right Key
-inputHandler (EventKey (SpecialKey KeyRight) _ _ _) gameState = scene Play (moveFn (\(x, y) -> (x + 1, y))) gameState
+inputHandler (EventKey (SpecialKey KeyRight) _ _ _) gameState =
+  scene Play (moveFn (\(x, y) -> (x + 1, y))) gameState
 -- Up Key
-inputHandler (EventKey (SpecialKey KeyUp) _ _ _) gameState = scene Play (moveFn (\(x, y) -> (x, y - 1))) gameState
+inputHandler (EventKey (SpecialKey KeyUp) _ _ _) gameState =
+  scene Play (moveFn (\(x, y) -> (x, y - 1))) gameState
 -- Down Key
-inputHandler (EventKey (SpecialKey KeyDown) _ _ _) gameState = scene Play (moveFn (\(x, y) -> (x, y + 1))) gameState
+inputHandler (EventKey (SpecialKey KeyDown) _ _ _) gameState =
+  scene Play (moveFn (\(x, y) -> (x, y + 1))) gameState
 
 inputHandler _ gameState = gameState
 
-scene s fn gameState = if unScene gameState == s then fn gameState else gameState
-moveFn dirFn gameState = gameState { unPacMan = move (unLevel gameState) dirFn (unPacMan gameState) }
+scene s fn gameState =
+  if unScene gameState == s then fn gameState else gameState
+moveFn dirFn gameState =
+  gameState { unPacMan = move (unLevel gameState) dirFn (unPacMan gameState) }
 
 move :: Grid -> ((Int, Int) -> (Int, Int)) -> PacMan -> PacMan
 move grid step pm@(PacMan nds d) = if upN /= finalN then mutate else pm
-  where
-    mutate = pm {
-      unPath     = [head nds, n upN, n finalN],
-      unDistance = rd
-    }
-    upN      = upcomingNode nds d
-    finalN   = final upN
-    -- new target is upcoming node. So new distance is distance to upcoming node which is the decimal value
-    rd       = d `mod'` 1
-    
-    n = uncurry Pn
-    final p = if canPass $ get $ step p then final (step p) else p
-    get (x, y) = level !! y !! x
-    canPass Empty             = True
-    canPass (Collectible _ _) = True
-    canPass _                 = False
-    level = getGridItems grid
+ where
+  mutate = pm { unPath = [head nds, n upN, n finalN], unDistance = rd }
+  upN    = upcomingNode nds d
+  finalN = final upN
+  -- new target is upcoming node. So new distance is distance to upcoming node which is the decimal value
+  rd     = d `mod'` 1
+
+  n      = uncurry Pn
+  final p = if canPass $ get $ step p then final (step p) else p
+  get (x, y) = level !! y !! x
+  canPass Empty             = True
+  canPass (Collectible _ _) = True
+  canPass _                 = False
+  level = getGridItems grid
