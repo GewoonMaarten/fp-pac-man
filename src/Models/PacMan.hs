f9bddbbc61bf5683db2c159248a75b53134dda4e
Maarten de Klerk <m.de.klerk@johancruijffarena.nl>
2019-10-31 10:50:27 +0100

    Added rotation to the drawing of pacman

--- src/Models/PacMan.hs
+++ src/Models/PacMan.hs
@@ -18,8 +18,10 @@ data Movement = Movement {
   unAnimStage :: AnimStage
 } deriving (Show)
 
-data Direction = MoveUp | MoveDown | MoveLeft | MoveRight deriving (Show, Enum, Bounded)
-data AnimStage = One | Two | Three | Four deriving (Show, Eq, Enum, Bounded)
+data Direction = MoveUp | MoveDown | MoveLeft | MoveRight 
+  deriving (Show, Enum)
+data AnimStage = One | Two | Three | Four 
+  deriving (Show, Eq, Enum, Bounded)
 
 initialPacMan :: PacMan
 initialPacMan = PacMan (P [Pn 9 7] 1) 0 (Just (Movement MoveLeft Three))
@@ -27,21 +29,25 @@ initialPacMan = PacMan (P [Pn 9 7] 1) 0 (Just (Movement MoveLeft Three))
 performPacManUpdate :: Float -> PacMan -> PacMan
 performPacManUpdate dt pm = pm { unPath = movePath (dt * 4) (unPath pm) }
 
-s = 0.6
--- ToDo add rotation
 showPacMan :: TextureSet -> PacMan -> Picture
-showPacMan (PacManTextureSet p _ _ _) (PacMan pm _ (Just (Movement dir One))) =
-  translate x (-y) $ scale s s $ p
-  where (x, y) = ((-180, -255) Pt.+ 20 Pt.* (actualLocation pm))
-showPacMan (PacManTextureSet _ p _ _) (PacMan pm _ (Just (Movement dir Two))) =
-  translate x (-y) $ scale s s $ p
-  where (x, y) = ((-180, -255) Pt.+ 20 Pt.* (actualLocation pm))
-showPacMan (PacManTextureSet _ _ p _) (PacMan pm _ (Just (Movement dir Three)))
-  = translate x (-y) $ scale s s $ p
-  where (x, y) = ((-180, -255) Pt.+ 20 Pt.* (actualLocation pm))
-showPacMan (PacManTextureSet _ _ _ p) (PacMan pm _ (Just (Movement dir Four)))
-  = translate x (-y) $ scale s s $ p
-  where (x, y) = ((-180, -255) Pt.+ 20 Pt.* (actualLocation pm))
+showPacMan ts pm@(PacMan _ _ (Just (Movement dir stage))) = let (x, y) = getLoc pm in 
+  translate x (-y) $ pacmManRotate dir $ pacManScale $ getTexture ts stage
+  where
+    getTexture :: TextureSet -> AnimStage -> Picture
+    getTexture ( PacManTextureSet p _ _ _ ) One   = p
+    getTexture ( PacManTextureSet _ p _ _ ) Two   = p
+    getTexture ( PacManTextureSet _ _ p _ ) Three = p
+    getTexture ( PacManTextureSet _ _ _ p ) Four  = p
+    getLoc :: PacMan -> (Float, Float)
+    getLoc pm = ((-180, -255) Pt.+ 20 Pt.* (actualLocation $ unPath pm))
+    pacManScale :: Picture -> Picture
+    pacManScale = let s = 0.6 in scale s s
+    pacmManRotate :: Direction -> Picture -> Picture
+    pacmManRotate MoveUp    = rotate (-90)
+    pacmManRotate MoveDown  = rotate 90
+    pacmManRotate MoveLeft  = rotate 180
+    pacmManRotate MoveRight = rotate 0
 
+showScore :: PacMan -> Picture
 showScore (PacMan _ s _) =
   translate 100 300 $ scale 0.2 0.2 $ color white $ text $ show s
