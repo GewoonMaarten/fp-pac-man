ebe7c57a1670c285b9287c1a52cfd307bebe9fc2
Maarten de Klerk <maarten.deklerk@gmail.com>
2019-11-06 13:36:11 +0100

    Fixed errors made during merging

--- src/Main.hs
+++ src/Main.hs
@@ -36,19 +36,9 @@ update dt = return . performUpdate dt
 performUpdate :: Float -> GameState -> GameState
 performUpdate dt gs = updateEnergizerTimers dt $ collectItems $ gs
     { unPacMan = updateAnimation dt $ performPacManUpdate dt (unPacMan gs)
-    , unGhosts = map
-                     (updateAnimation dt . performGhostUpdate (ghostFn gs) dt)
-                     (unGhosts gs)
+    , unGhosts = map gu $ unGhosts gs
     }
-
--- implement ghost choice using GameState at this level
-ghostFn gs p@(P (a : b : _) _) = P p $ newDistance p
   where
-    p = b : map (uncurry Pn) (follow (canPass . get) step $ f b)
-    f (Pn x y) = (x, y)
-    d        = directions (canPass . get) b a
-    (nx, ny) = head d
-    step (x, y) = (x + nx, y + ny)
-
-    get (x, y) = level !! y !! x
-    level = getGridItems $ unLevel gs
+    get = getGridItem $ unLevel gs
+    pm  = performPacManUpdate dt (unPacMan gs)
+    gu  = updateAnimation dt . performGhostUpdate (canPass . get) (unPath pm) dt
