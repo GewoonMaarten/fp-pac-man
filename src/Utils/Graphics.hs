dbc83e5625d1cb48ce15e958a7e626f0a9f82255
Maarten de Klerk <maarten.deklerk@gmail.com>
2019-11-06 16:33:58 +0100

    PacMan is now distributional

--- src/Utils/Graphics.hs
+++ src/Utils/Graphics.hs
@@ -1,6 +1,7 @@
 module Utils.Graphics where
 
 import           Graphics.Gloss
+import           Paths_PacMan
 
 data TextureSet = PacManTextureSet Picture Picture Picture Picture
   | DeathTextureSet Picture Picture Picture
@@ -15,38 +16,42 @@ data Textures = Textures {
   texturesPacManDeath :: TextureSet,
   textureBanner :: Picture
 }
+loadBMP' :: String -> IO Picture
+loadBMP' fileName = do
+  filePath <- getDataFileName fileName
+  loadBMP filePath
 
 loadTextures :: IO Textures
 loadTextures = do
   pacManTextures <-
     PacManTextureSet
-    <$> loadBMP "assets/pac_man_0.bmp"
-    <*> loadBMP "assets/pac_man_1.bmp"
-    <*> loadBMP "assets/pac_man_2.bmp"
-    <*> loadBMP "assets/pac_man_3.bmp"
+    <$> loadBMP' "assets/pac_man_0.bmp"
+    <*> loadBMP' "assets/pac_man_1.bmp"
+    <*> loadBMP' "assets/pac_man_2.bmp"
+    <*> loadBMP' "assets/pac_man_3.bmp"
   deathTextures <-
     DeathTextureSet
-    <$> loadBMP "assets/spr_pacdeath_0.bmp"
-    <*> loadBMP "assets/spr_pacdeath_1.bmp"
-    <*> loadBMP "assets/spr_pacdeath_2.bmp"
+    <$> loadBMP' "assets/spr_pacdeath_0.bmp"
+    <*> loadBMP' "assets/spr_pacdeath_1.bmp"
+    <*> loadBMP' "assets/spr_pacdeath_2.bmp"
   blinkyTextures <-
-    GhostTextureSet <$> loadBMP "assets/spr_ghost_red_0.bmp" <*> loadBMP
+    GhostTextureSet <$> loadBMP' "assets/spr_ghost_red_0.bmp" <*> loadBMP'
       "assets/spr_ghost_red_1.bmp"
   inkyTextures <-
-    GhostTextureSet <$> loadBMP "assets/spr_ghost_blue_0.bmp" <*> loadBMP
+    GhostTextureSet <$> loadBMP' "assets/spr_ghost_blue_0.bmp" <*> loadBMP'
       "assets/spr_ghost_blue_1.bmp"
   pinkyTextures <-
-    GhostTextureSet <$> loadBMP "assets/spr_ghost_pink_0.bmp" <*> loadBMP
+    GhostTextureSet <$> loadBMP' "assets/spr_ghost_pink_0.bmp" <*> loadBMP'
       "assets/spr_ghost_pink_1.bmp"
   clydeTextures <-
-    GhostTextureSet <$> loadBMP "assets/spr_ghost_orange_0.bmp" <*> loadBMP
+    GhostTextureSet <$> loadBMP' "assets/spr_ghost_orange_0.bmp" <*> loadBMP'
       "assets/spr_ghost_orange_1.bmp"
   afraidTextures <-
-    GhostTextureSet <$> loadBMP "assets/spr_afraid_0.bmp" <*> loadBMP
+    GhostTextureSet <$> loadBMP' "assets/spr_afraid_0.bmp" <*> loadBMP'
       "assets/spr_afraid_1.bmp"
-  lifeCounter   <- loadBMP "assets/spr_lifecounter_0.bmp"
-  cherryTexture <- loadBMP "assets/spr_cherry_0.bmp"
-  bannerTexture <- loadBMP "assets/spr_banner_0.bmp"
+  lifeCounter   <- loadBMP' "assets/spr_lifecounter_0.bmp"
+  cherryTexture <- loadBMP' "assets/spr_cherry_0.bmp"
+  bannerTexture <- loadBMP' "assets/spr_banner_0.bmp"
   return Textures
     { textureLifeCounter  = lifeCounter
     , textureCherry       = cherryTexture
